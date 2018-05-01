a78f10633dddefa83fe53373bc9da248ae4f93c8
Poonchy <Poonchys@gmail.com>
2018-05-01 14:18:23 -0400

    Re-added toTitleCase function

--- app.js
+++ app.js
@@ -91,10 +91,16 @@ clubAdvisorArray = [];
 objectToArray(advisorObject, clubAdvisorArray);
 clubEmailArray = [];
 objectToArray(emailObject, clubEmailArray);
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
 
 for (let i = 0; i < clubNamesArray.length; i++) {
   let noSpaceClub
   noSpaceClub = clubNamesArray[i];
+  clubNamesArray[i] = toTitleCase(clubNamesArray[i]);
   let s = 0;
   for (s; s < noSpaceClub.length; s++) {
       noSpaceClub = noSpaceClub.replace(" ", "");
